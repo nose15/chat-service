03a1103131429fec410704dd378dc0e694bf2151
nose15 <lukol12321@gmail.com>
2024-08-17 16:02:04 +0200

    Simple websocket server

--- src/main/java/org/studnia/Main.java
+++ src/main/java/org/studnia/Main.java
@@ -1,7 +1,27 @@
 package org.studnia;
 
+import org.glassfish.tyrus.server.Server;
+import org.studnia.endpoints.SocketHandler;
+
+import javax.websocket.DeploymentException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws DeploymentException {
+        Server server = new Server("localhost", 8080, "/", SocketHandler.class);
+        server.start();
+
+        try {
+            server.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Please press a key to stop the server.");
+            reader.readLine();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            server.stop();
+
+        }
     }
 }
---
+++ src/main/java/org/studnia/endpoints/SocketHandler.java
@@ -0,0 +1,38 @@
+package org.studnia.endpoints;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@ServerEndpoint(value = "/chat")
+public class SocketHandler {
+    private Session session;
+    private static final Set<SocketHandler> connections = new HashSet<SocketHandler>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+
+        System.out.println("Connection opened");
+        connections.add(this);
+    }
+
+    @OnClose
+    public void onClose(Session session) throws IOException {
+        this.session.close();
+        connections.remove(this);
+        System.out.println("Connection closed");
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        System.out.println("Error occured " + throwable.getMessage());
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        System.out.println(message);
+    }
+}

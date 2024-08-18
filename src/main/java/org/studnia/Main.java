83c1aff77e6d7bed436a883a6330e545a5524292
nose15 <lukol12321@gmail.com>
2024-08-18 16:03:25 +0200

    Updated old packages

--- src/main/java/org/studnia/Main.java
+++ src/main/java/org/studnia/Main.java
@@ -1,16 +1,20 @@
 package org.studnia;
 
+import jakarta.websocket.DeploymentException;
 import org.glassfish.tyrus.server.Server;
 import org.studnia.endpoints.SocketHandler;
 
-import javax.websocket.DeploymentException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws DeploymentException {
-        Server server = new Server("localhost", 8080, "/", SocketHandler.class);
-        server.start();
+    public static void main(String[] args) {
+        Server server = new Server("localhost", 8080, "/", null, SocketHandler.class);
+        try {
+            server.start();
+        } catch (DeploymentException e) {
+            throw new RuntimeException(e);
+        }
 
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
